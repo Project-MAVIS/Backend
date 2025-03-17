1d1dae3af453e6f363f0d3b83190827c0976ed70
Shantanu <shantanuwable2003@gmail.com>
2025-03-17 22:43:48 +0530

    Add gunicorn and uvicorn dependencies; create build script for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-9o8plk=l!k(7ytpb)6pm@30f(!9pu0*26_b$4imq$v#!$de#$#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "backend-h2o4.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "backend-h2o4.onrender.com", "127.0.0.1"]
 
 try:
     SERVER_PRIVATE_KEY, SERVER_PUBLIC_KEY = initialize_server_keys()
